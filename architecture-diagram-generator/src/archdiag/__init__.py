c756023fc9b51876b3f6e546d7bbdccaf3593f1f
Anupam Patel <patel.anupam02@gmail.com>
2026-08-16 17:11:02 +0530

    Revert "Do not merge: Use Case 2 app to copy into architecture-diagram-genera…"
    This reverts commit ad617393a0eb9f04315ac1d678abbf1f811c624d.

--- architecture-diagram-generator/src/archdiag/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Evidence-only architecture extraction from unstructured technical notes."""
-
-from archdiag.parse import interpret_notes
-from archdiag.schema import ArchitectureDiagram
-
-__all__ = ["interpret_notes", "ArchitectureDiagram"]
-__version__ = "0.1.0"
